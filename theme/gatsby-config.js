82630f8e307653df880d1733247d3faa3aa684b5
Scott Cooper <scttcper@gmail.com>
2019-07-22 23:15:44 -0700

    add theme

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,5 +1,20 @@
 const path = require('path');
 
+exports.onPreBootstrap = ({ store, reporter }) => {
+  const { program } = store.getState()
+
+  const dirs = [
+    path.join(program.directory, "content"),
+  ]
+
+  dirs.forEach(dir => {
+    if (!fs.existsSync(dir)) {
+      reporter.log(`creating the ${dir} directory`)
+      mkdirp.sync(dir)
+    }
+  })
+}
+
 module.exports = {
   siteMetadata: {
     title: 'Ghost',
@@ -10,7 +25,6 @@ module.exports = {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
   },
   plugins: [
-    'gatsby-plugin-sitemap',
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-filesystem',
@@ -60,24 +74,6 @@ module.exports = {
       resolve: 'gatsby-plugin-postcss',
       options: {
         postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-XXXX-Y',
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // IP anonymization for GDPR compliance
-        anonymize: true,
-        // Disable analytics for users with `Do Not Track` enabled
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
-        // Specifies what percentage of users should be tracked
-        sampleRate: 100,
-        // Determines how often site speed tracking beacons will be sent
-        siteSpeedSampleRate: 10,
       },
     },
   ],
